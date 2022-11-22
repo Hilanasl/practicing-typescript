186c1cd07eacf8d7191b92edca7cc19b73d29ed8
Hilana <hilana.sene@gmail.com>
2022-11-22 18:44:29 +0100

    tuple learning

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var person = {
     name: 'Hilana',
-    age: 35
+    age: 35,
+    hobbies: ['Series', 'Sports'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+// person.role[1] = 10
+var favoriteActivities;
+favoriteActivities = ['Series'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+} // toUpperCase works cause it's an array methode
--- app.ts
+++ app.ts
@@ -1,6 +1,23 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Hilana',
-    age: 35
+    age: 35,
+    hobbies: ['Series', 'Sports'],
+    role: [2, 'author']
 }
 
+// person.role.push('admin');
+// person.role[1] = 10
+
+let favoriteActivities: string[];
+favoriteActivities = ['Series'];
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+} // toUpperCase works cause it's an array methode
